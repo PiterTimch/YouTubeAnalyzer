075747a1f1f0d2b656acd190253fae070fa7f697
Petro <petrotymchuck@gmail.com>
2025-01-24 14:33:13 +0200

    Add repository

---
+++ DAL/Interfaces/IYouTubeStatisticsRepository.cs
@@ -0,0 +1,15 @@
+﻿using DAL.Enteties;
+
+namespace DAL.Interfaces
+{
+    public interface IYouTubeStatisticsRepository
+    {
+        ICollection<ChannelStatisticsEntity> GetAllChannels();
+        void AddChannel(ChannelStatisticsEntity item);
+        void DeleteChannel(ChannelStatisticsEntity item);
+
+        ICollection<VideoStatisticsEntity> GetAllVideos();
+        void AddVideo(VideoStatisticsEntity item);
+        void DeleteVideo(VideoStatisticsEntity item);
+    }
+}
---
+++ DAL/Repositories/YouTubeStatisticsRepository.cs
@@ -0,0 +1,51 @@
+﻿using DAL.Contexts;
+using DAL.Enteties;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class YouTubeStatisticsRepository : IYouTubeStatisticsRepository
+    {
+        public YouTubeStatisticsRepository() 
+        {
+            this._context = new YouTubeAnalyticsContext();
+        }
+
+        public void AddChannel(ChannelStatisticsEntity item)
+        {
+            this._context.Channels.Add(item);
+        }
+
+        public void AddVideo(VideoStatisticsEntity item)
+        {
+            this._context.Videos.Add(item);
+        }
+
+        public void DeleteChannel(ChannelStatisticsEntity item)
+        {
+            this._context.Channels.Remove(item);
+        }
+
+        public void DeleteVideo(VideoStatisticsEntity item)
+        {
+            this._context.Videos.Remove(item);
+        }
+
+        public ICollection<ChannelStatisticsEntity> GetAllChannels()
+        {
+            return this._context.Channels.ToList();
+        }
+
+        public ICollection<VideoStatisticsEntity> GetAllVideos()
+        {
+            return this._context.Videos.ToList();
+        }
+
+        private YouTubeAnalyticsContext _context;
+    }
+}
