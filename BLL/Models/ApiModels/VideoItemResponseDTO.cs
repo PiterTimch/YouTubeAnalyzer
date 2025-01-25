a65146e0e79a0a363c4d929aac58663ddfdceb96
Petro <petrotymchuck@gmail.com>
2025-01-25 14:00:26 +0200

    Add api DTO's

---
+++ BLL/Models/ApiModels/VideoItemResponseDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace BLL.Models.ApiModels
+{
+    public class VideoItemResponseDTO
+    {
+        public string Id { get; set; } = string.Empty;
+        public SnippetVideoDTO Snippet { get; set; }
+        public StatisticsVideoDTO Statistics { get; set; }
+    }
+
+    public class SnippetVideoDTO
+    {
+        public string Title { get; set; } = string.Empty;
+        public string ChannelId { get; set; } = string.Empty;
+    }
+
+    public class StatisticsVideoDTO
+    {
+        public string ViewCount { get; set; } = string.Empty;
+        public string LikeCount { get; set; } = string.Empty;
+        public string CommentCount { get; set; } = string.Empty;
+    }
+
+}
