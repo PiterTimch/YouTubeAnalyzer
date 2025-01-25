e0764f32ef1df65b0b882d053c3210af0aa6b986
Petro <petrotymchuck@gmail.com>
2025-01-25 07:47:32 +0200

    Add channel API entities

---
+++ DAL/Enteties/API-Entities/Channel/ChannelResponseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Enteties.API_Entities.Channel
+{
+    public class ChannelResponseEntity
+    {
+        public string Kind { get; set; } = string.Empty;
+        public string Etag { get; set; } = string.Empty;
+        public object PageInfo { get; set; } = new object();
+        public ICollection<ChannelItemResponseEntity> Items { get; set; }
+    }
+}
